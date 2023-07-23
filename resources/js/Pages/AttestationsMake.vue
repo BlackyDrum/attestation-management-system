5f9680dfbafda8a866ae34ec9f533a4162132fcb
BlackyDrum <ganiaytan1010@web.de>
2023-07-24 01:19:29 +0200

    Add the ``DataTable`` to make attestations

--- resources/js/Pages/AttestationsMake.vue
+++ resources/js/Pages/AttestationsMake.vue
@@ -4,6 +4,9 @@ import { Head, router, usePage } from '@inertiajs/vue3';
 import {onMounted, ref} from "vue";
 
 import Button from "primevue/button";
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Checkbox from "primevue/checkbox";
 
 import combine from "@/CombinedData.js";
 
@@ -15,12 +18,47 @@ defineProps({
 
 onMounted(() => {
     combinedData.value = combine(page.props.attestations);
-    console.log(combinedData.value);
+    subject_name.value = combinedData.value[0].subject_name;
+    tasks.value = combinedData.value[0].tasks;
+
+    const uniqueTitles = Array.from(new Set(tasks.value.flat().map((item) => item.title)));
+
+    const usersData = {};
+
+    tasks.value.flat().forEach((item) => {
+        if (!usersData[item.name]) {
+            usersData[item.name] = { Name: item.name };
+            uniqueTitles.forEach((title) => {
+                usersData[item.name][title] = false;
+            });
+        }
+    });
+
+    tasks.value.forEach((tasksArray, index) => {
+        tasksArray.forEach((task) => {
+            usersData[task.name][task.title] = task.checked;
+        });
+    });
+
+    userData.value = Object.values(usersData);
+    userData.value = userData.value.slice().sort((a,b) => {
+        const surnameA = a.Name.split(' ').slice(-1)[0]; // Get last word as surname
+        const surnameB = b.Name.split(' ').slice(-1)[0]; // Get last word as surname
+        return surnameA.localeCompare(surnameB);
+    })
+
+    headers.value = Object.keys(userData.value[0]).filter((key) => key !== "Name");
+    console.log(userData.value);
 })
 
 const page = usePage();
 
 let combinedData = ref(null);
+let subject_name = ref("");
+let tasks = ref([]);
+let userData = ref(null);
+let headers = ref(null);
+
 </script>
 
 <template>
@@ -28,7 +66,7 @@ let combinedData = ref(null);
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Make Attestations</h2>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Make Attestation for '{{subject_name}}'</h2>
         </template>
         <div class="mt-5 ml-5 text-gray-400 ">
             <Button @click="router.get('/attestations')" icon="pi pi-arrow-left" class="h-10" severity="secondary" label="Back"></Button>
@@ -36,7 +74,23 @@ let combinedData = ref(null);
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-
+                    <div>
+                        <DataTable filterMatchMode="Name" :value="userData" :paginator="true" :rows="10">
+                            <Column field="Name" header="Name"></Column>
+                            <Column v-for="header in headers" :field="header" :key="header">
+                                <template #header>
+                                    <div class="mx-auto">
+                                        <div>{{ header }}</div>
+                                    </div>
+                                </template>
+                                <template #body="{ index, field }">
+                                    <div class="flex justify-center items-center h-full">
+                                        <Checkbox v-model="userData[index][field]" :binary="true"/>
+                                    </div>
+                                </template>
+                            </Column>
+                        </DataTable>
+                    </div>
                 </div>
             </div>
         </div>
