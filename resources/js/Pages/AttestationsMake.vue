d11e4aa64b5dc82dac75acf68e2711f503356a1f
BlackyDrum <ganiaytan1010@web.de>
2023-07-25 00:56:36 +0200

    Add padding to Attestation ``DataTable``

--- resources/js/Pages/AttestationsMake.vue
+++ resources/js/Pages/AttestationsMake.vue
@@ -149,7 +149,7 @@ FilterService.register(FILTER.value, (value, filter) => {
         </div>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-4">
                     <div>
                         <DataTable showGridlines stripedRows ref="dt" :exportFilename="(subject_name + '_' + Date.now()).replaceAll(' ','_')" v-model:filters="filters" filterDisplay="row" :value="userData" :paginator="true" :rows="10">
                             <template #header>
