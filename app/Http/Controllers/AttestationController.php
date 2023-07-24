927ab7ab8c8ec903f2e31f1ec59cce6df0f24441
BlackyDrum <ganiaytan1010@web.de>
2023-07-25 00:45:30 +0200

    Refactor ``show`` function to reduce redundancy

--- app/Http/Controllers/AttestationController.php
+++ app/Http/Controllers/AttestationController.php
@@ -17,7 +17,7 @@ class AttestationController extends Controller
 {
     public function show(Request $request)
     {
-        $attestations = Attestation::query()
+        $attestationQuery = Attestation::query()
             ->join('semester', 'attestation.current_semester', '=', 'semester.id')
             ->join('attestation_tasks', 'attestation.id', '=', 'attestation_tasks.attestation_id')
             ->join('user_has_checked_task', 'user_has_checked_task.task_id', '=', 'attestation_tasks.id')
@@ -41,49 +41,17 @@ class AttestationController extends Controller
                 'attestation_tasks.id AS task_id',
                 'user_has_checked_task.id AS checked_id',
             ])
-            ->where('users.id','=',Auth::id())
-            ->orderBy('attestation_tasks.id')
-            ->get();
+            ->orderBy('attestation_tasks.id');
 
         if (!Auth::user()->admin) {
-            return Inertia::render('Attestations', [
-                'users' => [],
-                'semester' => [],
-                'attestations' => $attestations
-            ]);
+            $attestationQuery->where('users.id', '=', Auth::id());
         }
 
-        $attestations = Attestation::query()
-            ->join('semester', 'attestation.current_semester', '=', 'semester.id')
-            ->join('attestation_tasks', 'attestation.id', '=', 'attestation_tasks.attestation_id')
-            ->join('user_has_checked_task', 'user_has_checked_task.task_id', '=', 'attestation_tasks.id')
-            ->join('user_has_attestation', function ($join) {
-                $join->on('user_has_attestation.user_id', '=', 'user_has_checked_task.user_id')
-                    ->on('user_has_attestation.attestation_id', '=', 'attestation.id');
-            })
-            ->join('users', 'users.id', '=', 'user_has_attestation.user_id')
-            ->orderByRaw("SPLIT_PART(users.name,' ', -1)")
-            ->select([
-                'attestation.id',
-                'attestation.subject_name',
-                'attestation.subject_number',
-                'attestation.creator_id',
-                'semester.semester',
-                'attestation_tasks.title',
-                'attestation_tasks.description',
-                'user_has_attestation.user_id',
-                'users.name',
-                'user_has_checked_task.checked',
-                'attestation_tasks.id AS task_id',
-                'user_has_checked_task.id AS checked_id',
-            ])
-            ->orderBy('attestation_tasks.id')
-            ->get();
-
+        $attestations = $attestationQuery->get();
 
         return Inertia::render('Attestations', [
-            'users' => User::all(),
-            'semester' => Semester::all(),
+            'users' => Auth::user()->admin ? User::all() : [],
+            'semester' => Auth::user()->admin ? Semester::all() : [],
             'attestations' => $attestations,
         ]);
     }
