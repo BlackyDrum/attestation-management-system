b3053938976c247d1080966a305a679fc889394e
BlackyDrum <ganiaytan1010@web.de>
2023-07-26 21:23:33 +0200

    Auto format code

--- resources/js/Pages/AttestationsMake.vue
+++ resources/js/Pages/AttestationsMake.vue
@@ -1,7 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import {Head, router, useForm, usePage} from '@inertiajs/vue3';
-import {onMounted, ref} from "vue";
+import { Head, router, usePage } from '@inertiajs/vue3';
+import { onMounted, ref } from "vue";
 
 import Button from "primevue/button";
 import DataTable from 'primevue/datatable';
@@ -9,7 +9,7 @@ import Column from 'primevue/column';
 import Checkbox from "primevue/checkbox";
 import InputText from "primevue/inputtext";
 import Dialog from "primevue/dialog";
-import { FilterMatchMode, FilterService } from 'primevue/api';
+import { FilterService } from 'primevue/api';
 import { useToast } from 'primevue/usetoast';
 
 import combine from "@/CombinedData.js";
@@ -61,7 +61,7 @@ const page = usePage();
 const toast = useToast();
 
 const extractData = (data, index) => {
-    const keys = (Object.keys(data).filter(key => key.startsWith('task_id'))).map(key => key.replace('task_id_',''));
+    const keys = (Object.keys(data).filter(key => key.startsWith('task_id'))).map(key => key.replace('task_id_', ''));
 
     formData.value = formData.value.filter(obj => obj.user_id !== data.user_id);
 
@@ -75,7 +75,7 @@ const extractData = (data, index) => {
 }
 
 const handleFormSend = () => {
-    axios.patch('/attestations',{
+    axios.patch('/attestations', {
         tasks: formData.value
     })
         .then(response => {
@@ -118,7 +118,7 @@ const dt = ref();
 
 const FILTER = ref('FILTER');
 const filters = ref({
-    'Name': {value: null, matchMode: 'contains'},
+    'Name': { value: null, matchMode: 'contains' },
 });
 
 FilterService.register(FILTER.value, (value, filter) => {
@@ -138,33 +138,39 @@ FilterService.register(FILTER.value, (value, filter) => {
 </script>
 
 <template>
-    <Head title="Make Attestations"/>
+    <Head title="Make Attestations" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Make Attestation for '{{subject_name}}'</h2>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Make Attestation for
+                '{{ subject_name }}'</h2>
         </template>
         <div class="mt-5 ml-5 text-gray-400 ">
-            <Button @click="router.get('/attestations')" icon="pi pi-arrow-left" class="h-10" severity="secondary" label="Back"></Button>
+            <Button @click="router.get('/attestations')" icon="pi pi-arrow-left" class="h-10" severity="secondary"
+                label="Back"></Button>
         </div>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-4">
                     <div>
-                        <DataTable showGridlines stripedRows ref="dt" :exportFilename="(subject_name + '_' + Date.now()).replaceAll(' ','_')" v-model:filters="filters" filterDisplay="row" :value="userData" :paginator="true" :rows="10">
+                        <DataTable showGridlines stripedRows ref="dt"
+                            :exportFilename="(subject_name + '_' + Date.now()).replaceAll(' ', '_')"
+                            v-model:filters="filters" filterDisplay="row" :value="userData" :paginator="true" :rows="10">
                             <template #header>
                                 <div class="flex flex-wrap align-items-center justify-content-between gap-2">
                                     <div>
                                         <Button icon="pi pi-external-link" label="Export CSV" @click="exportCSV($event)" />
                                     </div>
                                     <div class="ml-auto mr-4">
-                                        <Button @click="handleFormSend" :disabled="formData.length === 0" icon="pi pi-save" severity="success" label="Save changes" />
+                                        <Button @click="handleFormSend" :disabled="formData.length === 0" icon="pi pi-save"
+                                            severity="success" label="Save changes" />
                                     </div>
                                 </div>
                             </template>
-                            <Column field="Name"  header="Name">
+                            <Column field="Name" header="Name">
                                 <template #filter="{ filterModel, filterCallback }">
-                                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter" placeholder="Search by name" />
+                                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()"
+                                        class="p-column-filter" placeholder="Search by name" />
                                 </template>
                             </Column>
                             <Column v-for="header in headers" :field="header" :key="header">
@@ -173,9 +179,9 @@ FilterService.register(FILTER.value, (value, filter) => {
                                         <div>{{ header }}</div>
                                     </div>
                                 </template>
-                                <template #body="{index, field,data }">
+                                <template #body="{ index, field, data }">
                                     <div class="flex justify-center items-center h-full">
-                                        <Checkbox v-model="data[field]" @change="extractData(data, index)" :binary="true"/>
+                                        <Checkbox v-model="data[field]" @change="extractData(data, index)" :binary="true" />
                                     </div>
                                 </template>
                             </Column>
@@ -186,15 +192,15 @@ FilterService.register(FILTER.value, (value, filter) => {
         </div>
 
         <!-- Dialogs -->
-        <Dialog v-model:visible="errorShow" header="Error"
-                class="w-1/2 max-md:w-full" position="topleft" :modal="false" :draggable="false">
+        <Dialog v-model:visible="errorShow" header="Error" class="w-1/2 max-md:w-full" position="topleft" :modal="false"
+            :draggable="false">
             <p class="text-red-600 font-medium">
                 {{ errorMessage }}
             </p>
         </Dialog>
 
-        <Dialog v-model:visible="successShow" header="Confirmation"
-                class="w-1/2 max-md:w-full" position="topleft" :modal="false" :draggable="false">
+        <Dialog v-model:visible="successShow" header="Confirmation" class="w-1/2 max-md:w-full" position="topleft"
+            :modal="false" :draggable="false">
             <p class="text-green-600 font-medium">
                 {{ successMessage }}
             </p>
