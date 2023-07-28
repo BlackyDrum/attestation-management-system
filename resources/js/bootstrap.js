043c7d9e9bf10a6fdc0786858d2f874c35f1c51f
BlackyDrum <ganiaytan1010@web.de>
2023-07-29 00:01:07 +0200

    Fix websocket connection error

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,9 +26,10 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    wsHost: window.location.hostname,
+    wsPort: 6001,
     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    forceTLS: false,
+    disableStats: true,
     enabledTransports: ['ws', 'wss'],
 });
