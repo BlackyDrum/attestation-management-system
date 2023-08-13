27613770747b6c935cbec16e7d7ccc38601b1ee3
BlackyDrum <ganiaytan1010@web.de>
2023-08-13 18:47:42 +0200

    Add ``ScrollPanel`` for tasks

--- resources/js/Components/ToDoList.vue
+++ resources/js/Components/ToDoList.vue
@@ -4,6 +4,7 @@ import {router, usePage} from "@inertiajs/vue3";
 
 import InputText from 'primevue/inputtext';
 import Checkbox from 'primevue/checkbox';
+import ScrollPanel from 'primevue/scrollpanel';
 
 
 defineProps({
@@ -75,8 +76,12 @@ const handleTaskChecking = (task) => {
         .then(() => {
             if (task.checked)
                 checked.value.push(task);
-            else
-                checked.value.splice(checked.value.findIndex((item) => item.id === task.id),1)
+            else {
+                let index = checked.value.findIndex((item) => item.id === task.id);
+                if (index !== -1)
+                    checked.value.splice(checked.value.findIndex((item) => item.id === task.id),1)
+            }
+
         })
         .catch(error => {
             window.toast.add({
@@ -103,8 +108,10 @@ const handleTaskDeletion = (task, index) => {
         }
     })
         .then(() => {
-            tasks.value.splice(index, 1);
-            checked.value.splice(checked.value.findIndex((item) => item.id === task.id),1);
+            tasks.value.splice(tasks.value.findIndex((item) => item.id === task.id), 1);
+            let index = checked.value.findIndex((item) => item.id === task.id);
+            if (index !== -1)
+                checked.value.splice(index,1);
         })
         .catch(error => {
             window.toast.add({
@@ -121,25 +128,42 @@ const handleTaskDeletion = (task, index) => {
 </script>
 
 <template>
-    {{checked}}
     <span class="p-input-icon-left w-full">
     <i class="pi pi-plus" />
     <InputText class="w-full" placeholder="Enter Your Todo..." v-model="taskInput" :disabled="processing" @keydown.enter="handleNewTask"/>
     </span>
-    <div class="group w-full px-3 py-5 text-lg text-white font-semibold border-b border-gray-700" v-for="(task, index) in tasks" :key="task.id">
-        <div class="grid grid-cols-[10%,80%,10%] break-words">
-            <div class="self-center">
-                <Checkbox :value="task.id" v-model="task.checked" :binary="true" :disabled="processing" @change="handleTaskChecking(task)"/>
-            </div>
-            <div class="self-center decoration-2 decoration-black" :class="{'line-through': task.checked}">
-                {{task.text}}
-            </div>
-            <div class="hidden group-hover:block">
-                <button class="pi pi-trash self-center mx-auto text-red-600 cursor-pointer" :disabled="processing" @click="handleTaskDeletion(task, index)"/>
+    <ScrollPanel class="custom-scroll-panel">
+        <div class="group w-full px-3 py-5 text-lg text-white font-semibold border-b border-gray-700" v-if="tasks.length !== 0" v-for="(task, index) in tasks" :key="task.id">
+            <div class="grid grid-cols-[10%,80%,10%] break-words">
+                <div class="self-center">
+                    <Checkbox :value="task.id" v-model="task.checked" :binary="true" :disabled="processing" @change="handleTaskChecking(task)"/>
+                </div>
+                <div class="self-center decoration-2 decoration-black" :class="{'line-through': task.checked}">
+                    {{task.text}}
+                </div>
+                <div class="hidden group-hover:block my-auto ml-auto">
+                    <button class="pi pi-trash self-center text-red-600 cursor-pointer" :disabled="processing" @click="handleTaskDeletion(task, index)"/>
+                </div>
             </div>
         </div>
-    </div>
+        <div v-else class="flex h-full">
+            <div class="mx-auto my-auto">
+                <div class="text-gray-700 text-center">
+                    <div class="pi pi-book custom-icon" style="font-size: 5rem"></div>
+                </div>
+                <div class="text-gray-500 text-center mt-4">
+                    Everything done!
+                </div>
+            </div>
+        </div>
+    </ScrollPanel>
     <div class="text-white">
         {{tasks.length - checked.length}} tasks left
     </div>
 </template>
+
+<style scoped>
+.custom-scroll-panel {
+    height: 20rem;
+}
+</style>
