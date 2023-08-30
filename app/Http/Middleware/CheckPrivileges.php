da9c7b876e1dc1b78355b3447036fc45c4f58336
BlackyDrum <ganiaytan1010@web.de>
2023-08-31 00:00:17 +0200

    Refactor middleware privilege check for more readability

--- app/Http/Middleware/CheckPrivileges.php
+++ app/Http/Middleware/CheckPrivileges.php
@@ -17,115 +17,40 @@ class CheckPrivileges
     public function handle(Request $request, Closure $next): Response
     {
         if (Auth::check() && Auth::user()->admin)
-        {
             return $next($request);
-        }
 
         $privileges = HandleInertiaRequests::get_privileges();
         $route = $request->route()->getName();
-        switch ($route) {
-            case 'send_notification':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_send_notification' && $privilege['checked'])
-                        return $next($request);
+
+        $privilegeMap = [
+            'send_notification' => 'can_send_notification',
+            'user' => 'can_access_user_page',
+            'delete_user' => 'can_delete_user',
+            'edit_user' => 'can_edit_user',
+            'create_user' => 'can_create_user',
+            'create_user_upload' => 'can_create_user',
+            'roles' => 'can_access_role_page',
+            'edit_roles' => 'can_edit_role',
+            'attestations' => 'can_access_subject_page',
+            'show_make_attestation' => 'can_access_attestation_page',
+            'make_attestation' => 'can_make_attestation',
+            'create_subject' => 'can_create_subject',
+            'edit_subject' => 'can_edit_subject',
+            'upload_user_subject' => 'can_edit_subject',
+            'delete_subject' => 'can_delete_subject',
+            'create_to_do' => 'can_create_todo',
+        ];
+
+        if (array_key_exists($route, $privilegeMap)) {
+            $requiredPrivilege = $privilegeMap[$route];
+
+            foreach ($privileges as $privilege) {
+                if ($privilege['privilege'] === $requiredPrivilege && $privilege['checked']) {
+                    return $next($request);
                 }
-                break;
-            case 'user':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_access_user_page' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'delete_user':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_delete_user' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'edit_user':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_edit_user' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'create_user':
-            case 'create_user_upload':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_create_user' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'roles':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_access_role_page' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'edit_roles':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_edit_role' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'attestations':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_access_subject_page' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'show_make_attestation':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_access_attestation_page' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'make_attestation':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_make_attestation' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'create_subject':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_create_subject' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'edit_subject':
-            case 'upload_user_subject':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_edit_subject' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'delete_subject':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_delete_subject' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
-            case 'create_to_do':
-                foreach ($privileges as $privilege)
-                {
-                    if ($privilege['privilege'] === 'can_create_todo' && $privilege['checked'])
-                        return $next($request);
-                }
-                break;
+            }
         }
 
-        abort(403,"Forbidden");
+        abort(403, "Forbidden");
     }
 }
