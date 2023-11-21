b0e598f11d73af4f6ed2baae3c09aa7616c97528
BlackyDrum <ganiaytan1010@web.de>
2023-11-21 19:33:24 +0100

    Show user upload button on subjects only if they are already created

--- resources/js/Pages/Attestations.vue
+++ resources/js/Pages/Attestations.vue
@@ -583,7 +583,7 @@ const handleSemesterChange = () => {
                     <error-message :show="errors.users">
                         {{ errors.users }}
                     </error-message>
-                    <div class="mt-2 flex flex-wrap gap-2">
+                    <div class="mt-2 flex flex-wrap gap-2" v-if="isEdit">
                         <FileUpload
                             accept="text/csv"
                             customUpload chooseLabel="CSV"
