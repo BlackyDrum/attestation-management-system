a3a0de77e326a64bdcdd67a61598de64ff5e78a8
BlackyDrum <ganiaytan1010@web.de>
2023-11-24 23:09:18 +0100

    Replace qualifier with imports

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\AttestationController;
+use App\Http\Controllers\AttestationsMakeController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MyAttestationsController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckPrivileges;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -20,45 +27,45 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::delete('/notifications', [\App\Http\Controllers\DashboardController::class, 'delete'])->middleware(['auth', 'verified']);
+Route::delete('/notifications', [DashboardController::class, 'delete'])->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'show'])->name('dashboard');
-    Route::get('/dashboard/data', [\App\Http\Controllers\DashboardController::class, 'getData']);
-    Route::patch('/dashboard/todo', [\App\Http\Controllers\DashboardController::class, 'checkToDo']);
-    Route::delete('/dashboard/todo', [\App\Http\Controllers\DashboardController::class, 'deleteToDo']);
+    Route::get('/dashboard', [DashboardController::class, 'show'])->name('dashboard');
+    Route::get('/dashboard/data', [DashboardController::class, 'getData']);
+    Route::patch('/dashboard/todo', [DashboardController::class, 'checkToDo']);
+    Route::delete('/dashboard/todo', [DashboardController::class, 'deleteToDo']);
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/my_attestations', [\App\Http\Controllers\MyAttestationsController::class, 'show'])->name('my_attestations');
+    Route::get('/my_attestations', [MyAttestationsController::class, 'show'])->name('my_attestations');
 
-    Route::middleware(\App\Http\Middleware\CheckPrivileges::class)->group(function() {
-        Route::post('/dashboard/todo', [\App\Http\Controllers\DashboardController::class, 'createToDo'])->name('create_to_do');
-        Route::get('/dashboard/users', [\App\Http\Controllers\DashboardController::class, 'getUsers'])->name('get_users');
+    Route::middleware(CheckPrivileges::class)->group(function() {
+        Route::post('/dashboard/todo', [DashboardController::class, 'createToDo'])->name('create_to_do');
+        Route::get('/dashboard/users', [DashboardController::class, 'getUsers'])->name('get_users');
 
-        Route::get('/attestations/{id}', [\App\Http\Controllers\AttestationsMakeController::class, 'show'])->name('show_make_attestation');
-        Route::patch('/attestations', [\App\Http\Controllers\AttestationsMakeController::class, 'make'])->name('make_attestation');
-        Route::get('/attestations', [\App\Http\Controllers\AttestationController::class, 'show'])->name('attestations');
-        Route::post('/attestations', [\App\Http\Controllers\AttestationController::class, 'create'])->name('create_subject');
-        Route::post('/attestations/users/upload', [\App\Http\Controllers\AttestationController::class, 'upload'])->name('upload_user_subject');
-        Route::post('/attestations/users/include', [\App\Http\Controllers\AttestationController::class, 'includeUsersToAttestation']);
-        Route::delete('/attestations', [\App\Http\Controllers\AttestationController::class, 'delete'])->name('delete_subject');
-        Route::put('/attestations', [\App\Http\Controllers\AttestationController::class, 'edit'])->name('edit_subject');
+        Route::get('/attestations/{id}', [AttestationsMakeController::class, 'show'])->name('show_make_attestation');
+        Route::patch('/attestations', [AttestationsMakeController::class, 'make'])->name('make_attestation');
+        Route::get('/attestations', [AttestationController::class, 'show'])->name('attestations');
+        Route::post('/attestations', [AttestationController::class, 'create'])->name('create_subject');
+        Route::post('/attestations/users/upload', [AttestationController::class, 'upload'])->name('upload_user_subject');
+        Route::post('/attestations/users/include', [AttestationController::class, 'includeUsersToAttestation']);
+        Route::delete('/attestations', [AttestationController::class, 'delete'])->name('delete_subject');
+        Route::put('/attestations', [AttestationController::class, 'edit'])->name('edit_subject');
 
-        Route::patch('/attestations/comment', [\App\Http\Controllers\AttestationController::class, 'updateComment'])->name('update_comment');
+        Route::patch('/attestations/comment', [AttestationController::class, 'updateComment'])->name('update_comment');
 
-        Route::get('/users', [\App\Http\Controllers\UserController::class, 'show'])->name('user');
-        Route::delete('/users', [\App\Http\Controllers\UserController::class, 'delete'])->name('delete_user');
-        Route::put('/users', [\App\Http\Controllers\UserController::class, 'edit'])->name('edit_user');
-        Route::post('/users', [\App\Http\Controllers\UserController::class, 'create'])->name('create_user');
-        Route::post('/users/upload', [\App\Http\Controllers\UserController::class, 'upload'])->name('create_user_upload');
+        Route::get('/users', [UserController::class, 'show'])->name('user');
+        Route::delete('/users', [UserController::class, 'delete'])->name('delete_user');
+        Route::put('/users', [UserController::class, 'edit'])->name('edit_user');
+        Route::post('/users', [UserController::class, 'create'])->name('create_user');
+        Route::post('/users/upload', [UserController::class, 'upload'])->name('create_user_upload');
 
-        Route::post('/notifications', [\App\Http\Controllers\DashboardController::class, 'send'])->name('send_notification');
+        Route::post('/notifications', [DashboardController::class, 'send'])->name('send_notification');
 
-        Route::get('roles', [\App\Http\Controllers\RoleController::class, 'show'])->name('roles');
-        Route::patch('roles', [\App\Http\Controllers\RoleController::class, 'update'])->name('edit_roles');
+        Route::get('roles', [RoleController::class, 'show'])->name('roles');
+        Route::patch('roles', [RoleController::class, 'update'])->name('edit_roles');
     });
 });
 
